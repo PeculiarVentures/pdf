37e2de6ef309635df301643e3dadacd64ad479f5
microshine <microshine@mail.ru>
2022-02-08 18:17:11 +0300

    fix: Type declaration

--- packages/core/src/encryption/PublicKeyEncryptionHandler.ts
+++ packages/core/src/encryption/PublicKeyEncryptionHandler.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="../pkijs.d.ts" />
+
 import * as asn1js from "asn1js";
 import { ByteStream } from "bytestreamjs";
 import { BufferSource, BufferSourceConverter } from "pvtsutils";
