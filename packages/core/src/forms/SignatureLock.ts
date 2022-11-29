6b030eaa8d87136f11e775cf128f0adf2f425001
microshine <microshine@mail.ru>
2022-11-29 22:55:26 +0300

    feat(core): support Lock Dictionary

--- packages/core/src/forms/SignatureLock.ts
+++ packages/core/src/forms/SignatureLock.ts
@@ -1,6 +1,6 @@
 import * as objects from "../objects";
 
-export enum SignatureLockActions {
+export enum SignatureLockAction {
   /**
    * All fields in the document
    */
@@ -15,43 +15,52 @@ export enum SignatureLockActions {
   exclude = "Exclude",
 }
 
+/**
+ * The signature field lock dictionary contains the names of form fields 
+ * whose values shall no longer be changed after this signature has been signed
+ */
 export class SignatureLockDictionary extends objects.PDFDictionary {
 
-  /**
-   * The type of PDF object that this dictionary describes
-   * 
-   * if present, shall be SigFieldLock for a signature field lock dictionary
-   */
-  @objects.PDFDictionaryField({
-    name: "Type",
-    type: objects.PDFName,
-    optional: true,
-    get: o => o.text
-  })
-  public type!: objects.PDFName | null;
+  public static readonly TYPE = "SigFieldLock";
 
   /**
-   * A name which, in conjunction with Fields, indicates the set 
-   * of fields that should be locked
+   * The type of PDF object that this dictionary describes; if present,
+   * shall be SigFieldLock for a signature field lock dictionary
    */
-  @objects.PDFDictionaryField({
-    name: "Action",
-    type: objects.PDFName,
-    optional: true,
-    get: o => o.text
-  })
-  public action!: SignatureLockActions | null;
+  @objects.PDFNameField("Type", true, SignatureLockDictionary.TYPE)
+  public Type!: string | null;
+
+  /**
+   * A name which, in conjunction with Fields, indicates the set of fields
+   * that should be locked. 
+   * 
+   * The value shall be one of the following:
+   * - `All` All fields in the document 
+   * - `Include` All fields specified in Fields 
+   * - `Exclude` All fields except those specified in Fields
+   */
+  @objects.PDFNameField("Action")
+  public Action!: SignatureLockAction;
 
   /**
    * An array of text strings containing field names
    * 
-   * Required if the value of Action is Include or Exclude
+   * @remarks Required if the value of Action is Include or Exclude
    */
-  @objects.PDFDictionaryField({
-    name: "Fields",
-    type: objects.PDFArray,
-    optional: true,
-  })
-  public fields!: objects.PDFArray | null;
+  @objects.PDFMaybeField("Fields", objects.PDFArray)
+  public Fields!: objects.Maybe<objects.PDFArray>;
+
+  /**
+   * The access permissions granted for this document. 
+   * 
+   * Valid values shall be:
+   * 1. No changes to the document are permitted; any change to the document shall invalidate the signature.
+   * 2. Permitted changes shall be filling in forms, instantiating page templates, and signing; other changes shall invalidate the signature. 
+   * 3. Permitted changes are the same as for 2, as well as annotation creation, deletion, and modification; other changes shall invalidate the signature.
+   * 
+   * @remarks PDF 2.0
+   */
+  @objects.PDFNumberField("P", true)
+  public P!: number | null;
 
 }
