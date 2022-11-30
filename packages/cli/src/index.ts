1ceb0fe8aa86cdcb3e48493f700b191f408f2490
microshine <microshine@mail.ru>
2022-11-30 19:47:13 +0300

    fix(cli): error on getEngine

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,9 +1,16 @@
-import * as commands from "./commands";
-
+import { Crypto } from "@peculiar/webcrypto";
+import * as core from "@peculiarventures/pdf-core";
 import { Command } from "commander";
+import * as pkijs from "pkijs";
+
+import * as commands from "./commands";
 
 const program = new Command("pdf");
 
+pkijs.setEngine("pdf", new core.PDFCryptoEngine({
+  name: "pdf",
+  crypto: new Crypto(),
+}));
 program.addCommand(commands.info);
 
 program.parse();
