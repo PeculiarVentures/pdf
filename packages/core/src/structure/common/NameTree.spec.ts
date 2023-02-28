18df79efb5ac2b813d58efc0247a35c8d8d8819c
microshine <microshine@mail.ru>
2023-02-28 21:58:54 +0100

    feat: implement setValue for NameTree

--- packages/core/src/structure/common/NameTree.spec.ts
+++ packages/core/src/structure/common/NameTree.spec.ts
@@ -1,48 +1,54 @@
 import * as assert from "node:assert";
 import { PDFDocument } from "../Document";
-import { PDFBoolean, PDFNumeric, PDFObject } from "../../objects";
+import { PDFBoolean, PDFDictionary, PDFNumeric, PDFObject, PDFTextString } from "../../objects";
 import { NameTree } from "./NameTree";
+import { ViewWriter } from "../../ViewWriter";
 
 context("NameTree", () => {
 
-  let nameTree: NameTree;
+  const doc = new PDFDocument();
 
-  before(() => {
-    const doc = new PDFDocument();
-    nameTree = new NameTree(doc.createDictionary( // Root node
-      ["Kids", doc.createArray(
-        doc.createDictionary( // Intermediate node
-          ["Limits", doc.createArray(doc.createString("AAA"), doc.createString("FFF"))],
-          ["Kids", doc.createArray(
-            doc.createDictionary( // Leaf node
-              ["Limits", doc.createArray(doc.createString("AAA"), doc.createString("CCC"))],
-              ["Names", doc.createArray(
-                doc.createString("AAA"), doc.createNumber(1),
-                doc.createString("BBB"), doc.createNumber(2),
-                doc.createString("CCC"), doc.createNumber(3),
-              )],
-            ).makeIndirect(),
-            doc.createDictionary( // Leaf node
-              ["Limits", doc.createArray(doc.createString("DDD"), doc.createString("FFF"))],
-              ["Names", doc.createArray(
-                doc.createString("DDD"), doc.createNumber(4),
-                doc.createString("EEE"), doc.createNumber(5),
-                doc.createString("FFF"), doc.createNumber(6),
-              )],
-            ).makeIndirect(),
-          )],
-        ).makeIndirect(),
-        doc.createDictionary( // Intermediate node
-          ["Limits", doc.createArray(doc.createString("GGG"), doc.createString("III"))],
-          ["Names", doc.createArray(
-            doc.createString("GGG"), doc.createNumber(7),
-            doc.createString("HHH"), doc.createNumber(8),
-            doc.createString("III"), doc.createNumber(9),
-          )],
-        ).makeIndirect(),
-      )],
-    ).makeIndirect());
-  });
+  const rootKids = new NameTree(doc.createDictionary( // Root node
+    ["Kids", doc.createArray(
+      doc.createDictionary( // Intermediate node
+        ["Limits", doc.createArray(doc.createString("AAA"), doc.createString("FFF"))],
+        ["Kids", doc.createArray(
+          doc.createDictionary( // Leaf node
+            ["Limits", doc.createArray(doc.createString("AAA"), doc.createString("CCC"))],
+            ["Names", doc.createArray(
+              doc.createString("AAA"), doc.createNumber(1),
+              doc.createString("BBB"), doc.createNumber(2),
+              doc.createString("CCC"), doc.createNumber(3),
+            )],
+          ).makeIndirect(),
+          doc.createDictionary( // Leaf node
+            ["Limits", doc.createArray(doc.createString("DDD"), doc.createString("FFF"))],
+            ["Names", doc.createArray(
+              doc.createString("DDD"), doc.createNumber(4),
+              doc.createString("EEE"), doc.createNumber(5),
+              doc.createString("FFF"), doc.createNumber(6),
+            )],
+          ).makeIndirect(),
+        )],
+      ).makeIndirect(),
+      doc.createDictionary( // Intermediate node
+        ["Limits", doc.createArray(doc.createString("GGG"), doc.createString("III"))],
+        ["Names", doc.createArray(
+          doc.createString("GGG"), doc.createNumber(7),
+          doc.createString("HHH"), doc.createNumber(8),
+          doc.createString("III"), doc.createNumber(9),
+        )],
+      ).makeIndirect(),
+    )],
+  ).makeIndirect());
+
+  const rootNames = new NameTree(doc.createDictionary( // Root node
+    ["Names", doc.createArray(
+      doc.createString("AAA"), doc.createNumber(1),
+      doc.createString("BBB"), doc.createNumber(2),
+      doc.createString("CCC"), doc.createNumber(3),
+    )],
+  ));
 
   context("findValue", () => {
     const tests: {
@@ -74,7 +80,7 @@ context("NameTree", () => {
 
     for (const t of tests) {
       it(t.name, async () => {
-        const res = nameTree.findValue(t.params);
+        const res = rootKids.findValue(t.params);
         if (t.want === null) {
           assert.strictEqual(res, t.want);
         } else {
@@ -86,12 +92,12 @@ context("NameTree", () => {
   });
 
   it("keys", () => {
-    const keys = nameTree.keys();
+    const keys = rootKids.keys();
     assert.deepStrictEqual(keys, ["AAA", "BBB", "CCC", "DDD", "EEE", "FFF", "GGG", "HHH", "III"]);
   });
 
   it("entries", () => {
-    const keys = nameTree.entries().map(o => {
+    const keys = rootKids.entries().map(o => {
       assert.ok(o instanceof PDFNumeric);
 
       return o.value;
@@ -136,13 +142,157 @@ context("NameTree", () => {
     for (const t of tests) {
       it(t.name, async () => {
         if (typeof t.want === "number") {
-          nameTree.getValue(t.params.key, t.params.type);
+          rootKids.getValue(t.params.key, t.params.type);
         } else {
           assert.throws(() => {
-            nameTree.getValue(t.params.key, t.params.type);
+            rootKids.getValue(t.params.key, t.params.type);
           }, t.want);
         }
       });
     }
   });
+
+  context("first", () => {
+    const tests: {
+      name: string;
+      tree: NameTree;
+      want: string | null;
+    }[] = [
+        {
+          name: "root with Kids",
+          tree: rootKids,
+          want: "AAA",
+        },
+        {
+          name: "root with Names",
+          tree: rootNames,
+          want: "AAA",
+        },
+      ];
+
+    for (const t of tests) {
+      it(t.name, async () => {
+        const res = t.tree.first();
+        if (t.want === null) {
+          assert.strictEqual(res, t.want);
+        } else {
+          assert.ok(res);
+          assert.strictEqual(res[0], t.want);
+        }
+      });
+    }
+  });
+
+  context("last", () => {
+    const tests: {
+      name: string;
+      tree: NameTree;
+      want: string | null;
+    }[] = [
+        {
+          name: "root with Kids",
+          tree: rootKids,
+          want: "III",
+        },
+        {
+          name: "root with Names",
+          tree: rootNames,
+          want: "CCC",
+        },
+      ];
+
+    for (const t of tests) {
+      it(t.name, async () => {
+        const res = t.tree.last();
+        if (t.want === null) {
+          assert.strictEqual(res, t.want);
+        } else {
+          assert.ok(res);
+          assert.strictEqual(res[0], t.want);
+        }
+      });
+    }
+  });
+
+  context("setValue", () => {
+
+    it("Root with Names", () => {
+      const doc = new PDFDocument();
+      const tree = NameTree.create(doc);
+      tree.Names = doc.createArray();
+
+      tree.setValue("BBB", doc.createBoolean(true));
+      tree.setValue("AAA", doc.createNumber(1));
+      tree.setValue("DDD", doc.createNull());
+      tree.setValue("CCC", doc.createHexString());
+      tree.setValue("EEE", doc.createString("text"));
+      tree.setValue("FFF", doc.createName("name"));
+      tree.setValue("GGG", doc.createDictionary());
+      tree.setValue("HHH", doc.createArray());
+
+      assert.strictEqual(tree.Names.length, 16);
+      assert.strictEqual(tree.Names.toString(), "[ (AAA), 1, (BBB), true, (CCC), 4 0 R, (DDD), null, (EEE), 5 0 R, (FFF), /name, (GGG), 6 0 R, (HHH), 7 0 R ]");
+    });
+
+    context("Root with Kids", () => {
+      let raw: ArrayBuffer;
+      let doc2: PDFDocument;
+      let tree: NameTree;
+
+      before(async () => {
+        const writer = new ViewWriter();
+        await doc.writePDF(writer);
+        raw = writer.toArrayBuffer();
+      });
+
+      beforeEach(async () => {
+        doc2 = new PDFDocument();
+        await doc2.fromPDF(raw);
+
+        const treeObj = doc2.getObject(rootKids.getIndirect()).value;
+        assert.ok(treeObj instanceof PDFDictionary);
+        tree = treeObj.to(NameTree);
+      });
+
+      it("before AAA, first", async () => {
+        tree.setValue("A", doc2.createNumber(1));
+
+        assert.strictEqual(tree.Kids?.get(0, NameTree).Limits?.get(0, PDFTextString).text, "A");
+        assert.strictEqual(tree.Kids?.get(0, NameTree).Limits?.get(1, PDFTextString).text, "FFF");
+        assert.strictEqual(tree.Kids?.get(0, NameTree).Kids?.get(0, NameTree).Limits?.get(0, PDFTextString).text, "A");
+        assert.strictEqual(tree.Kids?.get(0, NameTree).Kids?.get(0, NameTree).Limits?.get(1, PDFTextString).text, "CCC");
+        assert.strictEqual(tree.Kids?.get(0, NameTree).Kids?.get(0, NameTree).Names?.length, 8);
+      });
+
+      it("inside AAA-FFF, middle", async () => {
+        tree.setValue("D", doc2.createNumber(1));
+
+        assert.strictEqual(tree.Kids?.get(0, NameTree).Limits?.get(0, PDFTextString).text, "AAA");
+        assert.strictEqual(tree.Kids?.get(0, NameTree).Limits?.get(1, PDFTextString).text, "FFF");
+        assert.strictEqual(tree.Kids?.get(0, NameTree).Kids?.get(0, NameTree).Limits?.get(1, PDFTextString).text, "D");
+        assert.strictEqual(tree.Kids?.get(0, NameTree).Kids?.get(0, NameTree).Names?.length, 8);
+      });
+
+      it("between FFF and GGG from different groups, middle", async () => {
+        tree.setValue("G", doc2.createNumber(0));
+
+        assert.strictEqual(tree.Kids?.get(0, NameTree).Limits?.get(0, PDFTextString).text, "AAA");
+        assert.strictEqual(tree.Kids?.get(0, NameTree).Limits?.get(1, PDFTextString).text, "G");
+        assert.strictEqual(tree.Kids?.get(1, NameTree).Limits?.get(0, PDFTextString).text, "GGG");
+        assert.strictEqual(tree.Kids?.get(0, NameTree).Kids?.get(1, NameTree).Limits?.get(1, PDFTextString).text, "G");
+        assert.strictEqual(tree.Kids?.get(0, NameTree).Kids?.get(1, NameTree).Names?.length, 8);
+      });
+
+      it("after III, last", async () => {
+        tree.setValue("J", doc2.createNumber(0));
+
+        assert.strictEqual(tree.Kids?.get(1, NameTree).Limits?.get(0, PDFTextString).text, "GGG");
+        assert.strictEqual(tree.Kids?.get(1, NameTree).Limits?.get(1, PDFTextString).text, "J");
+        assert.strictEqual(tree.Kids?.get(1, NameTree).Names?.length, 8);
+      });
+
+    });
+
+  });
+
 });
--- packages/core/src/structure/common/NameTree.ts
+++ packages/core/src/structure/common/NameTree.ts
@@ -1,6 +1,6 @@
 import * as objects from "../../objects";
 
-export type NameTreeIterator = [string, objects.PDFObject];
+export type NameTreeIterator = [string, objects.PDFObject, NameTree];
 
 export type NameTreeType = typeof NameTree.EMPTY | typeof NameTree.ROOT | typeof NameTree.INTERMEDIATE | typeof NameTree.LEAF;
 
@@ -10,6 +10,8 @@ export class NameTree extends objects.PDFDictionary implements Iterable<NameTree
   public static ROOT = "root";
   public static INTERMEDIATE = "intermediate";
   public static LEAF = "leaf";
+
+  private parent?: NameTree;
 
   public get type(): NameTreeType {
     if (this.has("Names")) {
@@ -70,31 +72,19 @@ export class NameTree extends objects.PDFDictionary implements Iterable<NameTree
   @objects.PDFArrayField("Limits", true)
   public Limits!: objects.PDFArray | null;
 
-  /**
-   * Recursively adds key-value pairs from the NameTree to the provided array in
-   * the format of an array of tuples of strings and PDFObjects.
-   * @remarks
-   * The resulting array will contain all key-value pairs in the NameTree in
-   * lexicographical order.
-   * @param res - The array to add key-value pairs to
-   * @returns The array of key-value pairs
-   */
-  #toArray(res: Array<NameTreeIterator>) {
+  private leafArray(res: NameTree[] = []): NameTree[] {
     if (this.Names) {
-      for (let i = 0; i < this.Names.length; i += 2) {
-        const key = this.Names.get(i, objects.PDFTextString).text;
-        const value = this.Names.get(i + 1);
-        res.push([key, value]);
-      }
+      res.push(this);
     }
 
     if (this.Kids) {
-      for (const kid of this.Kids) {
-        if (!(kid instanceof objects.PDFDictionary)) {
-          continue;
-        }
+      for (let i = 0; i < this.Kids.length; i++) {
+        const kid = this.Kids.get(i, NameTree, true);
 
-        kid.to(NameTree).#toArray(res);
+        // Set parent for each child. This is needed for Limits update in setValue
+        kid.parent = this;
+
+        kid.leafArray(res);
       }
     }
 
@@ -105,8 +95,21 @@ export class NameTree extends objects.PDFDictionary implements Iterable<NameTree
    * Returns an array of key-value pairs from the NameTree object.
    * @returns Returns an array of key-value pairs
    */
-  public toArray(): Array<NameTreeIterator> {
-    return this.#toArray([]);
+  public toArray(): NameTreeIterator[] {
+    const res: NameTreeIterator[] = [];
+
+    const leafs = this.leafArray();
+    for (const leaf of leafs) {
+      if (leaf.Names) {
+        for (let i = 0; i < leaf.Names.length; i += 2) {
+          const key = leaf.Names.get(i, objects.PDFTextString).text;
+          const value = leaf.Names.get(i + 1);
+          res.push([key, value, leaf]);
+        }
+      }
+    }
+
+    return res;
   }
 
   [Symbol.iterator](): Iterator<NameTreeIterator, unknown, undefined> {
@@ -130,12 +133,7 @@ export class NameTree extends objects.PDFDictionary implements Iterable<NameTree
     };
   }
 
-  /**
-   * This method searches for a value within a NameTree based on a specified key.
-   * @param key Represents the key to be searched for in the tree
-   * @returns Returns the corresponding PDFObject, or null if no such value exists
-   */
-  public findValue(key: string): objects.PDFObject | null {
+  private findValueIterator(key: string): NameTreeIterator | null {
     // If this is an intermediate node with Limits, verify that the key is within the range
     if (this.compare(key)) {
       return null;
@@ -146,7 +144,7 @@ export class NameTree extends objects.PDFDictionary implements Iterable<NameTree
       for (let i = 0; i < this.Names.length; i += 2) {
         const keyItem = this.Names.get(i, objects.PDFTextString).text;
         if (keyItem === key) {
-          return this.Names.get(i + 1);
+          return [key, this.Names.get(i + 1), this];
         }
       }
     }
@@ -158,11 +156,26 @@ export class NameTree extends objects.PDFDictionary implements Iterable<NameTree
           continue;
         }
 
-        const res = kid.to(NameTree).findValue(key);
+        const res = kid.to(NameTree).findValueIterator(key);
         if (res) {
           return res;
         }
       }
+    }
+
+    return null;
+  }
+
+  /**
+   * This method searches for a value within a NameTree based on a specified key.
+   * @param key Represents the key to be searched for in the tree
+   * @returns Returns the corresponding PDFObject, or null if no such value exists
+   */
+  public findValue(key: string): objects.PDFObject | null {
+    const res = this.findValueIterator(key);
+
+    if (res) {
+      return res[1];
     }
 
     return null;
@@ -213,6 +226,14 @@ export class NameTree extends objects.PDFDictionary implements Iterable<NameTree
     return this.toArray().map(([, v]) => v);
   }
 
+  /**
+   * Compares Limits to a given key string.
+   * @param key The key string to compare with.
+   * @returns
+   * `-1` if the first limit is greater than the key,
+   * `0` if the key is within the limits, and
+   * `1` if the last limit is less than the key.
+   */
   public compare(key: string): number {
     if (this.Limits) {
       // Intermediate or Leaf
@@ -229,6 +250,135 @@ export class NameTree extends objects.PDFDictionary implements Iterable<NameTree
 
     // Root
     return 0;
+  }
+
+  /**
+   * Returns the first element of the tree. If the tree is empty, returns null.
+   * @returns The first element of the tree, or null if the tree is empty.
+   */
+  public first(): NameTreeIterator | null {
+    if (this.Limits) {
+      // Intermediate or Leaf
+      const first = this.Limits.get(0, objects.PDFTextString).text;
+
+      return this.findValueIterator(first);
+    }
+
+    // Root with Names
+    if (this.Names && this.Names.length) {
+      const key = this.Names.get(0, objects.PDFTextString).text;
+      const value = this.Names.get(1);
+
+      return [key, value, this];
+    }
+
+    // Root with Kids
+    if (this.Kids && this.Kids.length) {
+      return this.Kids.get(0, NameTree).first();
+    }
+
+    return null;
+  }
+
+  /**
+   * Returns the last element of the tree. If the tree is empty, returns null.
+   * @returns The first element of the tree, or null if the tree is empty.
+   */
+  public last(): NameTreeIterator | null {
+    if (this.Limits) {
+      // Intermediate or Leaf
+      const last = this.Limits.get(1, objects.PDFTextString).text;
+
+      return this.findValueIterator(last);
+    }
+
+    // Root with Names
+    if (this.Names && this.Names.length) {
+      const i = this.Names.length;
+      const key = this.Names.get(i - 2, objects.PDFTextString).text;
+      const value = this.Names.get(i - 1);
+
+      return [key, value, this];
+    }
+
+    // Root with Kids
+    if (this.Kids && this.Kids.length) {
+      return this.Kids.get(this.Kids.length - 1, NameTree).last();
+    }
+
+    return null;
+  }
+
+  /**
+   * Sets the value of the given key.
+   *
+   * @param key The key to set the value for.
+   * @param value The value to set for the given key.
+   * @throws `` if the given key cannot be set.
+   */
+  public setValue(key: string, value: objects.PDFObject): void {
+    const leafs = this.leafArray();
+
+    // Get a Leaf NameTree for the value adding
+    let target: NameTree | null = null;
+    for (const leaf of leafs) {
+      const compare = leaf.compare(key);
+      if (compare === 0) {
+        target = leaf;
+        break;
+      } else if (compare === -1) {
+        if (!target) {
+          target = leaf;
+        }
+        break;
+      }
+      target = leaf;
+    }
+
+    // Check the Leaf NameTree
+    if (!target || !target.Names) {
+      throw new Error("Cannot set value for the empty NameTree.");
+    }
+
+    // Convert Names to map
+    const map = new Map<string, objects.PDFObject>();
+    for (const [k, v] of target) {
+      map.set(k, v);
+    }
+    map.set(key, value);
+
+    // Sort keys
+    const sortedKeys = [...map.keys()].sort();
+
+    // Recreate array
+    const array = new objects.PDFArray;
+    for (const k of sortedKeys) {
+      const v = map.get(k);
+      if (!v) {
+        continue;
+      }
+
+      // Dictionary, Array and Strings shall be indirect objects
+      if (v instanceof objects.PDFDictionary || v instanceof objects.PDFArray || v instanceof objects.PDFTextString) {
+        v.makeIndirect();
+      }
+
+      array.push(new objects.PDFLiteralString(k), v as objects.PDFObjectTypes);
+    }
+    target.Names = array;
+
+    // Update Limits
+    let parent: NameTree | undefined = target;
+    while (parent && parent.Limits && parent.compare(key)) {
+      const min = parent.Limits.get(0, objects.PDFTextString);
+      if (min.text > key) {
+        min.text = key;
+      } else {
+        parent.Limits.get(1, objects.PDFTextString).text = key;
+      }
+
+      parent = parent.parent;
+    }
   }
 
 }
