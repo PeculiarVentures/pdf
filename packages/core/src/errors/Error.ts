72b1b7c22d75db163279db5275d447be402cf33a
microshine <microshine@mail.ru>
2023-03-15 15:30:07 +0100

    fix: conflict with es2020

--- packages/core/src/errors/Error.ts
+++ packages/core/src/errors/Error.ts
@@ -2,14 +2,14 @@ export class PDFError extends Error {
 
   public static readonly NAME: string = "PDFError";
 
-  public cause?: Error;
+  public inner?: Error;
 
   constructor(message: string, cause?: Error) {
     super(message);
 
     this.name = (this.constructor as typeof PDFError).NAME;
     if (cause) {
-      this.cause = cause;
+      this.inner = cause;
     }
   }
 
