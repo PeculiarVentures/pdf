c2da2e7ef2faebf07310d8175283822e126be6c8
microshine <microshine@mail.ru>
2024-01-03 12:01:14 +0100

    test: formatting

--- packages/font/src/FontFactory.spec.ts
+++ packages/font/src/FontFactory.spec.ts
@@ -2,7 +2,7 @@ import { DefaultFonts } from "./DefaultFonts";
 import { FontFactory } from "./FontFactory";
 
 it("test", () => {
-  const font= FontFactory.create(DefaultFonts.ZapfDingbats);
+  const font = FontFactory.create(DefaultFonts.ZapfDingbats);
   console.log(font);
   console.log(font.glyphs);
 });
