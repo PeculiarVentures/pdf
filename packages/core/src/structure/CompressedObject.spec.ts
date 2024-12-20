d39b6586218b480de602ce41c776f76a15f38e76
microshine <microshine@mail.ru>
2024-12-20 19:01:43 +0100

    test(structure): add type check for compressed object in CompressedObject tests

--- packages/core/src/structure/CompressedObject.spec.ts
+++ packages/core/src/structure/CompressedObject.spec.ts
@@ -1,5 +1,6 @@
 import { PDFDictionary, PDFNumeric } from "../objects";
 import { PDFDocument } from "./Document";
+import { PDFDocumentObjectTypes } from "./DocumentObject";
 
 describe("CompressedObject", () => {
   it("encode/decode", async () => {
@@ -18,13 +19,13 @@ describe("CompressedObject", () => {
     doc.update.append(dict2);
 
     const buf = await doc.toPDF();
-
     const doc2 = await PDFDocument.fromPDF(buf);
 
     const dict1_2 = doc2.getObject(objDict1.id);
     expect(dict1_2.id).toBe(objDict1.id);
     expect(dict1_2.generation).toBe(objDict1.generation);
     expect(dict1_2.type).toBe(objDict1.type);
+    expect(dict1_2.type).toBe(PDFDocumentObjectTypes.compressed);
     expect(dict1_2.value).toBeInstanceOf(PDFDictionary);
   });
 });
